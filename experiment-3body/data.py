157694aaffeda6a349f08d969557eb39f80a3dde
ZongyuWu97 <zongyuwu97@gmail.com>
2025-01-13 23:10:15 -0700

    3body update

--- experiment-3body/data.py
+++ experiment-3body/data.py
@@ -129,7 +129,7 @@ def random_config(nu=2e-1, min_radius=0.9, max_radius=1.2):
 
 
 ##### INTEGRATE AN ORBIT OR TWO #####
-def sample_orbits(timesteps=20, trials=5000, nbodies=3, orbit_noise=2e-1,
+def sample_orbits(timesteps=20, trials=5000, nbodies=3, orbit_noise=1e-1,
                   min_radius=0.9, max_radius=1.2, t_span=[0, 5], verbose=False, **kwargs):
     
     orbit_settings = locals()
